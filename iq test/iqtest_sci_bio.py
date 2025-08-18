c63213f5fc9c299b941f187759e3fee8831c9266
avikahub <singhavika245@gmail.com>
2025-08-18 21:32:43 +0530

    Update iqtest_sci_bio.py

--- iq test/iqtest_sci_bio.py
+++ iq test/iqtest_sci_bio.py
@@ -77,7 +77,12 @@ def iqtest_sci_bio():
           score[q['subject']] += 1
       count += 1
 
-  best_sub += (max(score, key=score.get))
+  best = max(score, key=score.get)   
+  max_score = score[best]            
+  if max_score < 7:
+    best_sub = ["No subject"]  
+  else:
+    best_sub = [best] 
 
 
   with open('data.json', 'r') as file:
@@ -89,6 +94,7 @@ def iqtest_sci_bio():
 
   with open('data.json', 'w') as data_file:
       json.dump(data, data_file, indent=4)
+
 
 
 

ceab040c0924fd2504df6379ebcb598bbbd22262
avikahub <singhavika245@gmail.com>
2025-08-20 13:34:03 +0530

    Update main1.py

--- text-interface/main1.py
+++ text-interface/main1.py
@@ -6,7 +6,6 @@ from iqtest_sci_math import *
 from iqtest_10th import *
 from eqtest import eqtest
 import time
-import sys
 
 best_sub = ''
 
@@ -28,7 +27,7 @@ print("""
 
 
 name = input("Hey There! Welcome to Direxa! Please enter your name\n")
-age = int(input(f"Hello {name}! Please enter your age.\n"))
+age = int(input("Hello, Please enter your age.\n"))
 grade = int(input("Please enter your class\n"))
 
 if grade > 10:
@@ -39,15 +38,10 @@ else:
 
 
 
-data = {
-    "name" : f"{name}",
-    "age": f'{age}',
-    "grade": f'{grade}',
-    "stream": f'{stream}',
-}
+data = {"name" : name, "age": age, "grade": grade, "stream": stream,}
 
 with open('data.json', 'w') as data_file:
-    json.dump(data, data_file, indent=4)
+    json.dump(data, data_file)
 
 time.sleep(1)
 
@@ -56,15 +50,15 @@ print("All done! You're all set up and ready to work. We'll first be taking your
 total = 25
 for i in range(total + 1):
     percent = int((i / total) * 100)
-    bar = ('#' * i).ljust(total)
-    sys.stdout.write(f"\r[{bar}] {percent}%")
-    sys.stdout.flush()
+    bar = "#" * i + " " * (total - i)  
+    print("\r[" + bar + "] " + str(percent) + "%", end="")  
     time.sleep(0.1)
+
 
 time.sleep(1)
 
 if stream == 'science':
-    math_bio = input("\n\nPCM or PCB?\n").lower()
+    math_bio = input("\nPCM or PCB?\n").lower()
     if math_bio == 'pcm':
         iqtest_sci_math()
     elif math_bio == 'pcb':
@@ -83,12 +77,11 @@ print("All done! Now, we'll be taking a quick EQ test based on your best scored 
 
 time.sleep(2)
 
-total = 25 
+total = 25
 for i in range(total + 1):
     percent = int((i / total) * 100)
-    bar = ('#' * i).ljust(total)
-    sys.stdout.write(f"\r[{bar}] {percent}%")
-    sys.stdout.flush()
+    bar = "#" * i + " " * (total - i)  
+    print("\r[" + bar + "] " + str(percent) + "%", end="")  
     time.sleep(0.1)
 
 time.sleep(2)
